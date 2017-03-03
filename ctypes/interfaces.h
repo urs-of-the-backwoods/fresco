1e545f03caca68e1ba967eaafedc6b0581f5d4fe
uotbw <uotbw@hgamer3d.org>
2017-03-03 21:16:33 +0100

    first run with cbor

--- ctypes/interfaces.h
+++ ctypes/interfaces.h
@@ -7,13 +7,75 @@ extern "C" {
 #endif
 
 //
+// Fresco Abstractions and Types
+//
+
+// a CBor Message, bytes and length
+
+typedef uint8_t* FrMsg;       
+typedef uint32_t FrMsgLength;
+
+/*
+    Components will be used later and they can be either an Item
+    or an Item Property. They have a type, which is denoted by
+    an uint64_t.
+*/
+
+typedef uint64_t FrComponentType;   
+typedef FrComponentType FrItemType;
+typedef FrComponentType FrPropertyType;
+
+/*
+    to get something, we can send a message to, we need a receiving item
+    and a function, which takes this item and a message. This will be 
+    the base abstraction of Fresco.
+
+    The items will have a type, called component type.
+
+    The combination of typed item with a message function can be seen as the
+    most simple receiver.
+
+*/
+
+typedef void* FrItem;
+typedef void (*FrMessageFn) (FrItem it, FrMsg m, FrMsgLength l);
+
+typedef struct {
+    FrItem item;
+    FrMessageFn sendFn;
+} FrReceiver;
+
+
+/*
+    giornata (object lib libraries) interface
+
+    A C++ object library exhibits this interface, to enable working 
+    with the exposed items.
+
+    There are some simple functions to create and destroy items. There
+    is a function to get the function for setting properties of an item.
+    And there is a function to set a property change callback. 
+*/
+
+typedef struct {
+
+    FrItem (*gioCreateItem) (FrItemType ct, FrMsg m, FrMsgLength l);
+    void (*gioDestroyItem) (FrItemType ct, FrItem it);
+    FrMessageFn (*gioGetMsgSender) (FrItemType ob, FrPropertyType pr); 
+    void (*gioRegisterMsgReceiver) (FrItemType ct, FrPropertyType pr, FrItem it, FrItem rcv, FrMessageFn);
+
+} FrGiornateInterface;
+
+
+
+
+
+
+//
 // generell Fresco types
 //
 
 typedef int32_t FrError;
-typedef unsigned char* FrMsg;       // A CBOR Message
-typedef uint32_t FrMsgLength;
-typedef uint64_t FrComponentType;   // A component can be an Object or a Property
 typedef void* FrItem;               // Object Library Item, created in C++ domain specific layer
 typedef void* FrReceiver;           // Msg Receiver 
 typedef void* FrEntity;             // Entity, created in Intoncao, Entity runtime, contains multiple components
@@ -24,7 +86,6 @@ typedef char* FrGiornataEnv;        // name of Giornata library environment vari
 typedef FrError (*FrMsgFn) (FrMsg m, FrMsgLength l);
 typedef FrError (*FrMsgEntityFn) (FrEntity e, FrComponentType ct, FrMsg m, FrMsgLength l);
 
-typedef FrError (*FrMsgItemFn) (FrItem it, FrMsg m, FrMsgLength l);
 typedef FrError (*FrMsgItemFn2) (FrItem it, FrComponentType ct, FrMsg m, FrMsgLength l);
 
 
@@ -55,19 +116,6 @@ typedef struct {
 
 } FrIntonacoInterface;
 
-//
-// giornata (object lib libraries) interface
-//
-
-typedef struct {
-
-    FrError (*gioCreateItem) (FrComponentType ct, FrMsg m, FrMsgLength l, FrItem *fri);
-    FrError (*gioDestroyItem) (FrComponentType ct, FrItem it);
-    FrError (*gioGetMsgSender) (FrComponentType ob, FrComponentType pr, FrMsgItemFn *f);                            // ct's: first is object, snd property'
-    FrError (*gioRegisterMsgReceiver) (FrComponentType ct, FrComponentType ev, FrItem it, FrReceiver rcv, FrMsgItemFn2);
-    char * (*gioErrorMessage) (FrError);
-
-} FrGiornateInterface;
 
 #ifdef __cplusplus
 }
